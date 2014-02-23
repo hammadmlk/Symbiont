8c2caf0c29d3e25ea9468f1c33bae4f353c8a2ae
Andrew Noyes <anoyes34@gmail.com>
2014-02-23 17:01:24 -0500

    s/Symbion/Symbiont/g

--- desktop/src/com/badlogic/symbiont/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/symbiont/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.badlogic.symbiont.SymbiontMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Symbion";
+		config.title = "Symbiont";
 		config.width = 480;
 		config.height = 800;
 		new LwjglApplication(new SymbiontMain(), config);
