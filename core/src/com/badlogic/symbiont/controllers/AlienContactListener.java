b7e0a34a48de457f7ac29b2fee6d58955bc2f614
Andrew Noyes <anoyes34@gmail.com>
2014-03-07 16:06:57 -0500

    lose conditions

--- core/src/com/badlogic/symbiont/controllers/AlienContactListener.java
+++ core/src/com/badlogic/symbiont/controllers/AlienContactListener.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
+import com.badlogic.symbiont.SymbiontMain;
+import com.badlogic.symbiont.models.GameState;
 import com.badlogic.symbiont.models.PhysicsEntityModel;
 
 public class AlienContactListener implements ContactListener
@@ -36,6 +38,9 @@ public class AlienContactListener implements ContactListener
         }
         if (other.entityType == PhysicsEntityModel.Type.PLANT) {
             other.toBeDestroyed = true;
+        }
+        if (other.entityType == PhysicsEntityModel.Type.GROUND) {
+            SymbiontMain.gameState.state = GameState.State.LOST;
         }
     }
 
