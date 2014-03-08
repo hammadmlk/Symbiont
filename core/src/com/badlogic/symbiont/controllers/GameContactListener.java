220b01cbffc6b1375e3e51497373179dac4e3abc
Ben <bch46@cornell.edu>
2014-03-07 19:52:36 -0500

    First attempt at deflector physics, just adds an impulse to the alien

--- core/src/com/badlogic/symbiont/controllers/GameContactListener.java
+++ core/src/com/badlogic/symbiont/controllers/GameContactListener.java
@@ -1,14 +1,24 @@
 package com.badlogic.symbiont.controllers;
 
-import com.badlogic.gdx.physics.box2d.*;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.symbiont.SymbiontMain;
 import com.badlogic.symbiont.models.GameState;
 import com.badlogic.symbiont.models.PhysicsEntityModel;
 
-public class GameContactListener implements com.badlogic.gdx.physics.box2d.ContactListener {
+public class GameContactListener implements ContactListener {
+    PhysicsEntityModel alien;
+    PhysicsEntityModel other;
+    boolean deflected;
 
     @Override
     public void beginContact(Contact contact) {
+    	deflected = false;
         Fixture fixtureA = contact.getFixtureA();
         Fixture fixtureB = contact.getFixtureB();
         PhysicsEntityModel a = (PhysicsEntityModel) fixtureA.getBody().getUserData();
@@ -38,11 +48,42 @@ public class GameContactListener implements com.badlogic.gdx.physics.box2d.Conta
         if (other.entityType == PhysicsEntityModel.Type.GROUND) {
             SymbiontMain.gameState.state = GameState.State.LOST;
         }
+        if (other.entityType == PhysicsEntityModel.Type.DEFLECTOR) {
+        	deflected = true;
+        }
+        
     }
 
     @Override
     public void endContact(Contact contact) {
-
+    	if (deflected) {
+    		deflected = false;
+    		
+    		// TODO this is a constant and should be stored somewhere else
+    		float desiredVel = 15;
+    		Vector2 vel = alien.body.getLinearVelocity();
+    		
+    		// Only add impulse if the alien isn't moving fast enough
+    		if (vel.len() > desiredVel) {
+    			return;
+    		}
+    		float velChange = desiredVel - vel.len();
+    		float impulse = alien.body.getMass() * velChange;
+    		
+    		float xdiff = SymbiontMain.gameState.deflectorEndpoints[1].x
+					- SymbiontMain.gameState.deflectorEndpoints[0].x;
+			float ydiff = SymbiontMain.gameState.deflectorEndpoints[1].y
+					- SymbiontMain.gameState.deflectorEndpoints[0].y;
+			
+			Vector2 impulseDir = new Vector2(ydiff, -xdiff);
+			impulseDir.nor();
+    		impulseDir.scl(impulse);
+    		if (impulseDir.y < 0) {
+    			impulseDir.scl(-1);
+    		}
+    		
+    		alien.body.applyLinearImpulse(impulseDir, alien.body.getWorldCenter(), true);
+        }
     }
 
     @Override
