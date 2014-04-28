45765ce574d9dfa1d5a23f702b2e3ac3d31fedd9
Ben <bch46@cornell.edu>
2014-04-28 03:49:49 -0400

    Formatting and style cleanup

--- core/src/com/badlogic/symbiont/views/DeflectorView.java
+++ core/src/com/badlogic/symbiont/views/DeflectorView.java
@@ -45,139 +45,128 @@ public class DeflectorView {
             }
         }
     }
-    
-    private void elastic(SpriteBatch batch, DeflectorEndpoint pointA, DeflectorEndpoint pointB, PhysicsEntityModel alien ){    	
-    	// Ball radius
-    	float r = alien.body.getFixtureList().first()
-    			.getShape().getRadius() * GameConstants.PIXELS_PER_METER;
-    	
-    	// swap points so pointA is always on right (bug fix)
-    	if(pointA.x< pointB.x){
-    		DeflectorEndpoint temp = pointA;
-    		pointA = pointB;
-    		pointB = temp;
-    	}
-    	
-    	// pointA coords
-    	float x1 = pointA.x;
-    	float y1 = pointA.y;
-    	
-    	// pointB coords
-    	float x2 = pointB.x;
-    	float y2 = pointB.y;
-    	
-    	// Ball coords
-    	float cx = alien.position.x;
-    	float cy = alien.position.y;
-    	
-    	// Gives you the angle between pointA and pointB
-    	float angleAB = (float) Math.atan2(y2 - y1, x2 - x1);
-    	
-    	// Precalculate cosine and sine
-    	float cosine = (float) Math.cos( angleAB );
-    	float sine = (float) Math.sin( angleAB );
 
-    	// Distances between ball and elastic centre
-    	float dbex = alien.position.x - (x2 + x1) / 2;
-    	float dbey = alien.position.y - (y2 + y1) / 2;
-    	
-    	// Find position of ball relative to the elastic (and it's rotation)
-    	float px1 = cosine * dbex + sine * dbey;
-    	float py1 = cosine * dbey - sine * dbex;
-    	
-    	float maxPerpDist = (float) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) / 2;
-    	
-    	
-    	// Check if distance to line surface is less than ball radius
-	    	if ( -py1 < r && px1 > -maxPerpDist && px1 < maxPerpDist  ) {
-	    	// Distances from ball to pointA
-	    	float dx1 = cx - x1;
-	    	float dy1 = cy - y1;
+    private void elastic(SpriteBatch batch, DeflectorEndpoint pointA, DeflectorEndpoint pointB,
+            PhysicsEntityModel alien) {
+        // Ball radius
+        float r = alien.body.getFixtureList().first().getShape().getRadius()
+                * GameConstants.PIXELS_PER_METER;
 
-	    	// Distances from ball to pointB
-	    	float dx2 = cx - x2;
-	    	float dy2 = cy - y2;
+        // swap points so pointA is always on right (bug fix)
+        if (pointA.x < pointB.x) {
+            DeflectorEndpoint temp = pointA;
+            pointA = pointB;
+            pointB = temp;
+        }
 
-	    	// Calculate exact distances (LEFT)
-	    	float h1 = (float) Math.sqrt(dx1*dx1 + dy1*dy1);
-	    	float p1 = (float) Math.sqrt(h1*h1 - r*r);
+        // pointA coords
+        float x1 = pointA.x;
+        float y1 = pointA.y;
 
-	    	// Calculate exact distances (RIGHT)
-	    	float h2 = (float) Math.sqrt(dx2*dx2 + dy2*dy2);
-	    	float p2 = (float) Math.sqrt(h2*h2 - r*r);
+        // pointB coords
+        float x2 = pointB.x;
+        float y2 = pointB.y;
 
-	    	// Dont continue if the ball touches points
-			if ( h1 < r || h2 < r ) {
-				return;
-			}
+        // Ball coords
+        float cx = alien.position.x;
+        float cy = alien.position.y;
 
-	    	// Calculate angles (LEFT)
-	    	float a1 = (float) Math.atan2(dy1, dx1);
-	    	float b1 = (float) Math.asin(r / h1);
+        // Gives you the angle between pointA and pointB
+        float angleAB = (float) Math.atan2(y2 - y1, x2 - x1);
 
-	    	// Calculate angles (RIGHT)
-	    	// Small adjustment needed to find tangent
-	    	// on opposite side of ball (hense -r = opposite radius)
-	    	float a2 = (float) Math.atan2(dy2, dx2);
-	    	float b2 = (float) Math.asin(-r / h2); 
+        // Precalculate cosine and sine
+        float cosine = (float) Math.cos(angleAB);
+        float sine = (float) Math.sin(angleAB);
 
-	    	// Tangent/Ball intersection coordinates (LEFT)
-	    	float ix1 = (float) Math.cos(a1+b1) * p1 + x1;
-	    	float iy1 = (float) Math.sin(a1+b1) * p1 + y1;
+        // Distances between ball and elastic centre
+        float dbex = alien.position.x - (x2 + x1) / 2;
+        float dbey = alien.position.y - (y2 + y1) / 2;
 
-	    	// Tangent/Ball intersection coordinates (RIGHT)
-	    	float ix2 = (float) Math.cos(a2+b2) * p2 + x2;
-	    	float iy2 = (float) Math.sin(a2+b2) * p2 + y2;
+        // Find position of ball relative to the elastic (and it's rotation)
+        float px1 = cosine * dbex + sine * dbey;
+        float py1 = cosine * dbey - sine * dbex;
 
-	    	//Midpoint between the two tangent points (ix1,iy1) and (ix2,iy2)
-	    	float mpx = (ix1 + ix2) / 2;
-	    	float mpy = (iy1 + iy2) / 2;
-	    	// Angle of acceleration
-	    	float accAngle = (float) Math.atan2(mpy - cy, mpx - cx);
+        float maxPerpDist = (float) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2;
 
-	    	//distance between pointA and pointB
-	    	float d = (float) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        // Check if distance to line surface is less than ball radius
+        if (-py1 < r && px1 > -maxPerpDist && px1 < maxPerpDist) {
+            // Distances from ball to pointA
+            float dx1 = cx - x1;
+            float dy1 = cy - y1;
 
-	    	// Power is dependent on how far the elastic's been stretched 
-	    	//	and the distance between pointA and pointB 
-	    	float power = py1/50 ;//* (1+(10*d/GameConstants.VIRTUAL_WIDTH));
-	    	//System.out.println("power1: "+py1 /50);
+            // Distances from ball to pointB
+            float dx2 = cx - x2;
+            float dy2 = cy - y2;
 
-	    	//Change in velocity x and y 
-	    	float dvx = (float) Math.cos(accAngle) * power;
-	    	float dvy = (float) Math.sin(accAngle) * power*2;
+            // Calculate exact distances (LEFT)
+            float h1 = (float) Math.sqrt(dx1 * dx1 + dy1 * dy1);
+            float p1 = (float) Math.sqrt(h1 * h1 - r * r);
 
-	    	//
-	    	Vector2 velo = alien.body.getLinearVelocity();
-	    	velo.x-=dvx;
-	    	velo.y-=dvy;
-	    	// maximum speed limit
-	    	velo.x = (velo.x<0)? Math.max(-25, velo.x):Math.min(25, velo.x);
-	    	velo.y = (velo.y<0)? Math.max(-25, velo.y):Math.min(25, velo.y);	    	
-	    	alien.body.setLinearVelocity(velo.x, velo.y);
+            // Calculate exact distances (RIGHT)
+            float h2 = (float) Math.sqrt(dx2 * dx2 + dy2 * dy2);
+            float p2 = (float) Math.sqrt(h2 * h2 - r * r);
 
-	    	/*
-	    	//Impulse (changeInVelocity*mass)
-	    	Vector2 impulse = new Vector2( 
-	    			-dvx*alien.body.getMass(), 
-	    			-dvy*alien.body.getMass()
-			);	    	
-	    	alien.body.applyLinearImpulse(
-	    			impulse, alien.body.getWorldCenter(), true);
-	    	*/
+            // Dont continue if the ball touches points
+            if (h1 < r || h2 < r) {
+                return;
+            }
 
-	    	// Draw lines
-	    	drawline(batch, atlasRegion.originalHeight/2, x1, y1, ix1, iy1);   	
-	    	drawline(batch, atlasRegion.originalHeight/2, x2, y2, ix2, iy2);
-    	} else{
-    		// No collision detected... simply draw a line from pointA to pointB
-    		drawline(batch, atlasRegion.originalHeight/2, x1, y1, x2, y2);
-    	}
+            // Calculate angles (LEFT)
+            float a1 = (float) Math.atan2(dy1, dx1);
+            float b1 = (float) Math.asin(r / h1);
+
+            // Calculate angles (RIGHT)
+            // Small adjustment needed to find tangent
+            // on opposite side of ball (hense -r = opposite radius)
+            float a2 = (float) Math.atan2(dy2, dx2);
+            float b2 = (float) Math.asin(-r / h2);
+
+            // Tangent/Ball intersection coordinates (LEFT)
+            float ix1 = (float) Math.cos(a1 + b1) * p1 + x1;
+            float iy1 = (float) Math.sin(a1 + b1) * p1 + y1;
+
+            // Tangent/Ball intersection coordinates (RIGHT)
+            float ix2 = (float) Math.cos(a2 + b2) * p2 + x2;
+            float iy2 = (float) Math.sin(a2 + b2) * p2 + y2;
+
+            // Midpoint between the two tangent points (ix1,iy1) and (ix2,iy2)
+            float mpx = (ix1 + ix2) / 2;
+            float mpy = (iy1 + iy2) / 2;
+            // Angle of acceleration
+            float accAngle = (float) Math.atan2(mpy - cy, mpx - cx);
+
+            // distance between pointA and pointB
+            float d = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+            // Power is dependent on how far the elastic's been stretched
+            // and the distance between pointA and pointB
+            float power = py1 / 50;// * (1+(10*d/GameConstants.VIRTUAL_WIDTH));
+
+            // Change in velocity x and y
+            float dvx = (float) Math.cos(accAngle) * power;
+            float dvy = (float) Math.sin(accAngle) * power * 2;
+
+            //
+            Vector2 velo = alien.body.getLinearVelocity();
+            velo.x -= dvx;
+            velo.y -= dvy;
+            // maximum speed limit
+            velo.x = (velo.x < 0) ? Math.max(-25, velo.x) : Math.min(25, velo.x);
+            velo.y = (velo.y < 0) ? Math.max(-25, velo.y) : Math.min(25, velo.y);
+            alien.body.setLinearVelocity(velo.x, velo.y);
+
+            // Draw lines
+            drawline(batch, atlasRegion.originalHeight / 2, x1, y1, ix1, iy1);
+            drawline(batch, atlasRegion.originalHeight / 2, x2, y2, ix2, iy2);
+        } else {
+            // No collision detected... simply draw a line from pointA to pointB
+            drawline(batch, atlasRegion.originalHeight / 2, x1, y1, x2, y2);
+        }
     }
-    
-    //draws a line between the two points (x1,y1) and (ix1, iy1).
-    private void drawline(SpriteBatch batch, float height, float x1, float y1, float ix1, float iy1){
-    	float angle = (float) Math.toDegrees(Math.atan2(iy1 - y1, ix1 - x1));
+
+    // draws a line between the two points (x1,y1) and (ix1, iy1).
+    private void drawline(SpriteBatch batch, float height, float x1, float y1, float ix1, float iy1) {
+        float angle = (float) Math.toDegrees(Math.atan2(iy1 - y1, ix1 - x1));
         
         batch.draw(
                 atlasRegion,                                                         // Texture atlasRegion
@@ -191,7 +180,6 @@ public class DeflectorView {
                 1,                                                                   // float scaleY
                 angle                                                                // float rotation
         );
-        
     }
 
 }
